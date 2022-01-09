13d436092b98453a27569334a78974de180232a2
Alexander <sansanichick@gmail.com>
2022-01-09 19:10:41 +1000

    schema fix

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import DreamOrm from './main';
  * ! plan for basic features
  * * 1. we should start form connection (at first it can be simple)
  * ? 2. reflex about Entities (think how Entities should be look like)
- * * 3. Alexsandar can start working on translation and Andrei can start working on Entities and Validation
+ * * 3. Alexander can start working on translation and Andrei can start working on Entities and Validation
  * ? 4. difficult queries
  * * 5.  
  */
