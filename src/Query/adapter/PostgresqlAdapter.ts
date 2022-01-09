c9ad9116dd02cc2928e5af43d882f760046e6e42
Loksli17 <ami0504@mail.ru>
2022-01-09 18:22:15 +1000

    postgres query createDatabase sql text

--- src/Query/adapter/PostgresqlAdapter.ts
+++ src/Query/adapter/PostgresqlAdapter.ts
@@ -1,5 +1,32 @@
+import { Pool } from 'pg';
 
 
 export default class PostgreesqlAdapter {
 
+    public pool: Pool;
+
+    constructor(pool: Pool) {
+        this.pool = pool;
+    }
+
+    /**
+     * 
+    CREATE DATABASE test
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+     */
+
+    public createDatabase() {
+        // pg.
+    }
+
+    
+    public createTable() {
+        
+    }
 }
