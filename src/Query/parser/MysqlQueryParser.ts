37f442cee9bf6036e92fcb60e062e022c9ab7e8e
Loksli17 <ami0504@mail.ru>
2022-03-17 00:11:23 +1000

    MySQLQueryParser start

---
+++ src/Query/parser/MysqlQueryParser.ts
@@ -0,0 +1,59 @@
+import { QueryObject } from "../QueryBuilder";
+
+
+
+export default class MysqlQueryParser {
+
+    private queryObject: QueryObject = {};
+    private sql        : string      = '';
+
+
+    private parseColumns(): void {
+
+        if(this.queryObject.columns == undefined) {
+            this.sql += '*';
+            return;
+        };
+
+        this.queryObject.columns.forEach((column: string, index: number) => {
+            if(index != this.queryObject.columns!.length - 1){
+                this.sql += `${column}, `;
+            }
+            this.sql += `${column}`;
+        });
+
+    }
+
+
+    private parseLimit(): void {
+
+        if(this.queryObject.limit == undefined && this.queryObject.offset == undefined) return;
+
+        if(this.queryObject.limit == undefined && this.queryObject.offset) throw new Error('Offset can not be exist without Limit!')
+    
+        if(this.queryObject.limit && this.queryObject.offset == undefined) {
+            this.sql += this.queryObject.limit;
+            return;
+        }
+
+        if(this.queryObject.limit && this.queryObject.offset) {
+            this.sql += `${this.queryObject.limit}, ${this.queryObject.offset}`;
+            return;
+        }
+    }
+
+
+    public parseSelect(queryObject: QueryObject): string {
+
+        this.queryObject = queryObject;
+
+        this.sql += 'SELECT ';
+        this.parseColumns();
+        this.sql += " FROM ";
+        this.sql += this.queryObject.tableName
+
+        this.parseLimit();
+
+        return this.sql;
+    }
+}
